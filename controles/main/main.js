9caf99a82d0c150d0aea8de0b7104bf48e9d1a08
oberdanbrito <oberdan@craos.net>
2019-06-27 10:55:25 -0300

    Removido o arquivo campos.main.top desnecessário

--- controles/main/main.js
+++ controles/main/main.js
@@ -220,3 +220,12 @@ function logout() {
     });
 
 }
+
+var campos_top = [
+    { type: "settings", labelAlign: "left", position: "label-top"},
+    {type: "container", name: "logo", offsetLeft: "8", offsetTop: "10", inputWidth: 137},
+    {type: "newcolumn"},
+    {type: "label", name: "identificacao", offsetTop: "33", offsetLeft: "50",  format: "IdentificacaoUnidade"},
+    {type: "newcolumn"},
+    {type: "template", name: "informacoesUsuario",  offsetTop: "33",  format: "reInformacoesUsuario"}
+];
