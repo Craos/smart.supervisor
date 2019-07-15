c304facb5bea0c4b811433a5184ff15a91039c4a
oberdanbrito <oberdan@craos.net>
2019-07-15 14:32:50 -0300

    Inclusão dos botoões das barras de comandos utilizadas pelos recursos

--- js/supervisor/toolbar.js
+++ js/supervisor/toolbar.js
@@ -1,72 +1,77 @@
 let toolbars = {
-    unidade: {
-        geral:[
-            {type: 'button', id: 'atualizar', img: 'atualizar.png', text: 'Atualizar'}
-        ],
-        usuario:[
-            {type: 'button', id: 'atualizar', img: 'atualizar.png', text: 'Atualizar'}
-        ],
-        moradores:[
-            {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-            {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
-            {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
-            {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
-            {type: "separator", id: "sep1"},
-            {type: 'button', id: 'reserva', img: 'atualizar.png', text: 'Reservar espaço'}
-        ],
-        veiculos:[
-            {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-            {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
-            {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
-            {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
-            {type: "separator", id: "sep1"},
-        ],
-        funcionarios:[
-            {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-            {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
-            {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
-            {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
-            {type: "separator", id: "sep1"},
-        ],
-        hospedes:[
-            {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-            {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
-            {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
-            {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
-            {type: "separator", id: "sep1"},
-        ],
-        pets:[
-            {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-            {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
-            {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
-            {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
-            {type: "separator", id: "sep1"},
-        ],
-        preautorizados:[
-            {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-            {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
-            {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
-            {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
-            {type: "separator", id: "sep1"},
-        ]
-    },
-    personal: [
+    cadastro:[
+        {type: 'button', id: 'atualizar', img: 'atualizar.png', text: 'Atualizar'}
+    ],
+    usuario:[
+        {type: 'button', id: 'atualizar', img: 'atualizar.png', text: 'Atualizar'}
+    ],
+    moradores:[
         {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+        {type: 'button', id: 'reserva', img: 'atualizar.png', text: 'Reservar espaço'}
+    ],
+    veiculos:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
         {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
         {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
         {type: "separator", id: "sep1"},
     ],
-    portaria:[
+    funcionarios:[
         {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+    ],
+    hospedes:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+    ],
+    pets:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+    ],
+    preautorizados:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
         {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
         {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
         {type: "separator", id: "sep1"},
     ],
     notificacoes:[
         {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
-        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Adicionar'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+    ],
+    mudancasreformas:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+    ],
+    solicitacoesocorrencias:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
+        {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
+        {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
+        {type: "separator", id: "sep1"},
+    ],
+    historicoatividades:[
+        {type: 'button', id: 'novo', img: 'atualizar.png', text: 'Novo'},
+        {type: 'button', id: 'salvar', img: 'atualizar.png', text: 'Salvar'},
         {type: 'button', id: 'foto', img: 'atualizar.png', text: 'Obter foto'},
         {type: 'button', id: 'remover', img: 'atualizar.png', text: 'Remover'},
         {type: "separator", id: "sep1"},
